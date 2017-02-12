164534c6af836f30f4750b229432b2144ef76406
Matt Hicks <matt@outr.com>
2017-02-12 01:24:21 -0600

    Switched to Circe instead of uPickle and added parsing support.

--- build.sbt
+++ build.sbt
@@ -6,11 +6,19 @@ crossScalaVersions := List("2.12.1", "2.11.8")
 sbtVersion := "0.13.13"
 scalacOptions ++= Seq("-unchecked", "-deprecation")
 resolvers += Resolver.sonatypeRepo("releases")
+fork := true
+
+val circeVersion = "0.7.0"
 
 libraryDependencies ++= Seq(
 	"com.outr" %% "scribe" % "1.3.2",
 	"com.eed3si9n" %% "gigahorse-asynchttpclient" % "0.2.0",
-  "com.lihaoyi" %% "upickle" % "0.4.4",
 	"org.scalactic" %% "scalactic" % "3.0.1",
 	"org.scalatest" %% "scalatest" % "3.0.1" % "test"
 )
+
+libraryDependencies ++= Seq(
+	"io.circe" %% "circe-core",
+	"io.circe" %% "circe-generic",
+	"io.circe" %% "circe-parser"
+).map(_ % circeVersion)
--- src/main/scala/com/outr/arango/ArangoHTTP.scala
+++ src/main/scala/com/outr/arango/ArangoHTTP.scala
@@ -4,19 +4,33 @@ import java.nio.charset.Charset
 
 import gigahorse.Config
 import gigahorse.support.asynchttpclient.Gigahorse
-import upickle.Js
+import io.circe.generic.auto._
+import io.circe.parser._
+import io.circe.syntax._
 
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
 
 class ArangoHTTP(url: String) {
+  private val entryRegex = """"(.+)":(.+)""".r
+  private val snakeRegex = """_([a-z])""".r
+
   def authenticate(username: String, password: String): Future[AuthenticationResponse] = {
     val http = Gigahorse.http(Config())
     val json = s"""{"username":"$username","password":"$password"}"""
     val request = Gigahorse.url(s"$url/_open/auth").post(json, Charset.forName("utf-8"))
     val future = http.run(request).map { response =>
       val body = response.bodyAsString
-      SnakePickle.read[AuthenticationResponse](body)
+      val json = entryRegex.replaceAllIn(body, (regexMatch) => {
+        val key = snakeRegex.replaceAllIn(regexMatch.group(1), (snakeMatch) => {
+          snakeMatch.group(1).toUpperCase
+        })
+        s""""$key": ${regexMatch.group(2)}"""
+      })
+      decode[AuthenticationResponse](json) match {
+        case Left(error) => throw new RuntimeException(s"JSON decoding error: $json", error)
+        case Right(result) => result
+      }
     }
     future.onComplete { _ =>
       http.close()
@@ -25,17 +39,23 @@ class ArangoHTTP(url: String) {
   }
 
   object query {
-    // /_db/mydb/
-    def apply(token: String, db: String, query: String): Future[String] = {
+    def parse(token: String, db: String, query: String): Future[ParseResult] = {
       val http = Gigahorse.http(Config())
-      val json = upickle.default.write(QueryRequest(query))
+      val json = QueryRequest(query).asJson.spaces2
       val request = Gigahorse
-        .url(s"$url/_db/$db/_api/cursor")
+        .url(s"$url/_db/$db/_api/query")
         .post(json, Charset.forName("utf-8"))
         .withHeaders("Authorization" -> s"bearer $token")
-      val future = http.run(request).map { response =>
-        val body = response.bodyAsString
-        body
+      val future = http.processFull(request).map { response =>
+        if (response.status != 200) {
+          ParseResult(error = true, code = response.status, parsed = false, collections = Nil, bindVars = Nil, ast = Nil)
+        } else {
+          val body = response.bodyAsString
+          decode[ParseResult](body) match {
+            case Left(error) => throw new RuntimeException(s"JSON decoding error: $json", error)
+            case Right(result) => result
+          }
+        }
       }
       future.onComplete { _ =>
         http.close()
@@ -47,7 +67,7 @@ class ArangoHTTP(url: String) {
 
     def cursor(token: String, db: String, query: String, count: Boolean, batchSize: Int): Future[String] = {
       val http = Gigahorse.http(Config())
-      val json = upickle.default.write(QueryCursorRequest(query, count, batchSize))
+      val json = QueryCursorRequest(query, count, batchSize).asJson.spaces2
       val request = Gigahorse
         .url(s"$url/_db/$db/_api/cursor")
         .post(json, Charset.forName("utf-8"))
@@ -68,21 +88,14 @@ class ArangoHTTP(url: String) {
   case class AuthenticationResponse(jwt: String, mustChangePassword: Boolean)
 }
 
-object SnakePickle extends upickle.AttributeTagged {
-  def camelToSnake(s: String): String = {
-    val res = s.split("(?=[A-Z])", -1).map(_.toLowerCase).mkString("_")
-    res
-  }
+case class ParseResult(error: Boolean,
+                       code: Int,
+                       parsed: Boolean,
+                       collections: List[String],
+                       bindVars: List[String],
+                       ast: List[ParsedAST])
 
-  override def CaseR[T: Reader, V](f: T => V,
-                                        names: Array[String],
-                                        defaults: Array[Js.Value]): Reader[V] = {
-    super.CaseR[T, V](f, names.map(camelToSnake), defaults)
-  }
-
-  override def CaseW[T: Writer, V](f: V => Option[T],
-                                        names: Array[String],
-                                        defaults: Array[Js.Value]): Writer[V] = {
-    super.CaseW[T, V](f, names.map(camelToSnake), defaults)
-  }
-}
+case class ParsedAST(`type`: String,
+                     name: Option[String],
+                     id: Option[Int],
+                     subNodes: Option[List[ParsedAST]])
