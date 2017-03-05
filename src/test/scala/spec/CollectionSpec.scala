2ff8ec08ad346ef03c36a251e9a2581c8c4f6b47
Matt Hicks <matt@outr.com>
2017-03-04 19:04:14 -0600

    Drastically simplified the tests in CollectionSpec.

--- src/test/scala/spec/CollectionSpec.scala
+++ src/test/scala/spec/CollectionSpec.scala
@@ -1,135 +1,83 @@
 package spec
 
-import com.outr.arango.ArangoSession
+import com.outr.arango.{ArangoCollection, ArangoDB, ArangoSession}
 import org.scalatest.{AsyncWordSpec, Matchers}
 import io.circe.generic.auto._
 
+import scala.concurrent.Future
+
 class CollectionSpec extends AsyncWordSpec with Matchers {
+  private var session: ArangoSession = _
+  private var db: ArangoDB = _
+  private var test: ArangoCollection = _
+
   "Collections" should {
+    "create the session" in {
+      ArangoSession.default.map { s =>
+        session = s
+        db = session.db("_system")
+        test = db.collection("test")
+        s.token shouldNot be("")
+      }
+    }
     "create a new collection" in {
-      ArangoSession.default.flatMap { session =>
-        val dbSession = session.db("_system")
-        val collection = dbSession.collection("test")
-        val future = collection.create().map { response =>
-          response.error should be(false)
-        }
-        future.onComplete { _ =>
-          session.instance.dispose()
-        }
-        future
+      test.create().map { response =>
+        response.error should be(false)
       }
     }
     "insert a document" in {
-      ArangoSession.default.flatMap { session =>
-        val dbSession = session.db("_system")
-        val collection = dbSession.collection("test")
-        val future = collection.document.create(User("John Doe", 30), waitForSync = true, returnNew = true).map { response =>
-          response.`new` should be(Some(User("John Doe", 30)))
-        }
-        future.onComplete { _ =>
-          session.instance.dispose()
-        }
-        future
+      test.document.create(User("John Doe", 30), waitForSync = true, returnNew = true).map { response =>
+        response.`new` should be(Some(User("John Doe", 30)))
       }
     }
     "get collection information" in {
-      ArangoSession.default.flatMap { session =>
-        val dbSession = session.db("_system")
-        val collection = dbSession.collection("test")
-        val future = collection.information().map { response =>
-          response.name should be("test")
-          response.`type` should be(2)
-          response.status should be(3)
-        }
-        future.onComplete { _ =>
-          session.instance.dispose()
-        }
-        future
+      test.information().map { response =>
+        response.name should be("test")
+        response.`type` should be(2)
+        response.status should be(3)
       }
     }
     "get collection properties" in {
-      ArangoSession.default.flatMap { session =>
-        val dbSession = session.db("_system")
-        val collection = dbSession.collection("test")
-        val future = collection.properties().map { response =>
-          response.waitForSync should be(false)
-        }
-        future.onComplete { _ =>
-          session.instance.dispose()
-        }
-        future
+      test.properties().map { response =>
+        response.waitForSync should be(false)
       }
     }
     "get collection count" in {
-      ArangoSession.default.flatMap { session =>
-        val dbSession = session.db("_system")
-        val collection = dbSession.collection("test")
-        val future = collection.count().map { response =>
-          response.name should be("test")
-          response.`type` should be(2)
-          response.status should be(3)
-          response.count should be(1)
-        }
-        future.onComplete { _ =>
-          session.instance.dispose()
-        }
-        future
+      test.count().map { response =>
+        response.name should be("test")
+        response.`type` should be(2)
+        response.status should be(3)
+        response.count should be(1)
       }
     }
     "get collection revision" in {
-      ArangoSession.default.flatMap { session =>
-        val dbSession = session.db("_system")
-        val collection = dbSession.collection("test")
-        val future = collection.revision().map { response =>
-          response.name should be("test")
-          response.`type` should be(2)
-          response.status should be(3)
-          response.revision shouldNot be("0")
-        }
-        future.onComplete { _ =>
-          session.instance.dispose()
-        }
-        future
+      test.revision().map { response =>
+        response.name should be("test")
+        response.`type` should be(2)
+        response.status should be(3)
+        response.revision shouldNot be("0")
       }
     }
     "list all collections" in {
-      ArangoSession.default.flatMap { session =>
-        val dbSession = session.db("_system")
-        val collection = dbSession.collection("test")
-        val future = collection.list().map { response =>
-          val collectionNames = response.result.map(_.name).toSet
-          collectionNames should be(Set("test"))
-        }
-        future.onComplete { _ =>
-          session.instance.dispose()
-        }
-        future
+      test.list().map { response =>
+        val collectionNames = response.result.map(_.name).toSet
+        collectionNames should be(Set("test"))
       }
     }
     "truncate the collection" in {
-      ArangoSession.default.flatMap { session =>
-        val dbSession = session.db("_system")
-        val collection = dbSession.collection("test")
-        val future = collection.truncate().map { response =>
-          response.error should be(false)
-        }
-        future.onComplete { _ =>
-          session.instance.dispose()
-        }
-        future
+      test.truncate().map { response =>
+        response.error should be(false)
       }
     }
     "drop the new collection" in {
-      ArangoSession.default.flatMap { session =>
-        val dbSession = session.db("_system")
-        val collection = dbSession.collection("test")
-        val future = collection.drop().map { response =>
-          response.error should be(false)
-        }
-        future.onComplete { _ =>
-          session.instance.dispose()
-        }
-        future
+      test.drop().map { response =>
+        response.error should be(false)
+      }
+    }
+    "dispose the session" in {
+      Future {
+        session.instance.dispose()
+        session.instance.isDisposed should be(true)
       }
     }
   }
