f6c56a94b4d6540e13014232f12f375266f2d360
Matt Hicks <matt@outr.com>
2017-05-11 11:35:03 -0500

    Created ReplicationMonitor to simplify looking for changes.

--- driver/src/main/scala/com/outr/arango/rest/LoggerState.scala
+++ driver/src/main/scala/com/outr/arango/rest/LoggerState.scala
@@ -16,4 +16,49 @@ case class ConnectedClient(serverId: String, lastServedTick: String, time: Strin
 
 case class LoggerFollow(active: Boolean, lastIncluded: Long, lastTick: Long, checkMore: Boolean, events: List[LogEvent])
 
-case class LogEvent(tick: String, `type`: Int, tid: String, database: String, cid: String, cname: String, data: Json)
+case class LogEvent(tick: String, `type`: Int, tid: String, database: String, cid: String, cname: String, data: Json) {
+  lazy val eventType: EventType = EventType(`type`)
+}
+
+sealed abstract class EventType(val value: Int)
+
+object EventType {
+  case object LoggerStopped extends EventType(1000)
+  case object LoggerStarted extends EventType(1001)
+
+  case object CollectionCreated extends EventType(2000)
+  case object CollectionDropped extends EventType(2001)
+  case object CollectionRenamed extends EventType(2002)
+  case object CollectionPropChanged extends EventType(2003)
+
+  case object IndexCreated extends EventType(2100)
+  case object IndexDropped extends EventType(2101)
+
+  case object TransactionStarted extends EventType(2200)
+  case object TransactionCommitted extends EventType(2201)
+
+  case object DocumentUpsert extends EventType(2300)
+  case object EdgeUpsert extends EventType(2301)
+  case object Deletion extends EventType(2302)
+
+  private val items = List(
+    LoggerStopped,
+    LoggerStarted,
+    CollectionCreated,
+    CollectionDropped,
+    CollectionRenamed,
+    CollectionPropChanged,
+    IndexCreated,
+    IndexDropped,
+    TransactionStarted,
+    TransactionCommitted,
+    DocumentUpsert,
+    EdgeUpsert,
+    Deletion
+  )
+
+  private val map = items.map(et => et.value -> et).toMap
+
+  def get(value: Int): Option[EventType] = map.get(value)
+  def apply(value: Int): EventType = map(value)
+}
