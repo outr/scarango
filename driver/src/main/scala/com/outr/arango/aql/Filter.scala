a21a7e567f29f1fb9178e2bc21882bbfbbcfe938
Matt Hicks <matt@outr.com>
2019-11-21 08:56:24 -0600

    Added filter OR

--- driver/src/main/scala/com/outr/arango/aql/Filter.scala
+++ driver/src/main/scala/com/outr/arango/aql/Filter.scala
@@ -6,6 +6,9 @@ class Filter(left: Query, condition: String, right: Query) {
   def &&(filter: Filter): Filter = {
     new Filter(build(), "&&", filter.build())
   }
+  def ||(filter: Filter): Filter = {
+    new Filter(build(), "||", filter.build())
+  }
 
   def build(): Query = Query(s"${left.value} $condition ${right.value}", left.args ++ right.args)
 }
