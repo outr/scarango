ce4213289c70c2988252fd5bea70223b575c7784
Matt Hicks <matt@outr.com>
2019-11-22 11:57:48 -0600

    Release 2.2.1

--- driver/src/main/scala/com/outr/arango/aql/CollectStart.scala
+++ driver/src/main/scala/com/outr/arango/aql/CollectStart.scala
@@ -1,7 +1,5 @@
 package com.outr.arango.aql
 
-import com.outr.arango.aql
-
 object CollectStart {
-  def WITH(collectWith: CollectWith): CollectWith.Partial = new aql.CollectWith.Partial(collectWith)
+  def WITH(collectWith: CollectWith): CollectWith.Partial = new CollectWith.Partial(collectWith)
 }
