591024801b18554151c8f0889dfaa57c5d018b79
Matt Hicks <matt@matthicks.com>
2022-02-14 10:27:31 -0600

    Updated dependencies

--- driver/src/main/scala/com/outr/arango/core/ArangoDBConfig.scala
+++ driver/src/main/scala/com/outr/arango/core/ArangoDBConfig.scala
@@ -6,12 +6,12 @@ import scala.concurrent.duration._
 
 case class ArangoDBConfig(username: String = Profig("arangodb.username").opt[String].orNull,
                           password: String = Profig("arangodb.password").opt[String].orNull,
-                          ssl: Boolean = Profig("arangodb.ssl").as[Boolean](false),
+                          ssl: Boolean = Profig("arangodb.ssl").asOr[Boolean](false),
                           timeout: FiniteDuration = Profig("arangodb.timeout").opt[Long].map(_.millis).orNull,
-                          acquireHostList: Boolean = Profig("arangodb.acquireHostList").as[Boolean](false),
-                          chunkSize: Int = Profig("arangodb.chunkSize").as[Int](-1),
+                          acquireHostList: Boolean = Profig("arangodb.acquireHostList").asOr[Boolean](false),
+                          chunkSize: Int = Profig("arangodb.chunkSize").asOr[Int](-1),
                           connectionTtl: FiniteDuration = Profig("arangodb.connectionTtl").opt[Long].map(_.millis).orNull,
-                          hosts: List[Host] = Profig("arangodb.hosts").as[List[Host]](List(Host())),
+                          hosts: List[Host] = Profig("arangodb.hosts").asOr[List[Host]](List(Host())),
                           keepAliveInterval: FiniteDuration = Profig("arangodb.keepAliveInterval").opt[Long].map(_.millis).orNull,
                           loadBalancingStrategy: LoadBalancingStrategy = Profig("arangodb.loadBalancingStrategy").opt[LoadBalancingStrategy].getOrElse(LoadBalancingStrategy.None),
-                          maxConnections: Int = Profig("arangodb.maxConnections").as[Int](1))
+                          maxConnections: Int = Profig("arangodb.maxConnections").asOr[Int](1))
