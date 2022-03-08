8cf66fb1e7f1716684165e932d4270f9aece310d
Matt Hicks <matt@matthicks.com>
2022-03-08 08:53:34 -0600

    Added defaults to AuditRecord and sessionRef

--- core/src/main/scala/com/outr/arango/audit/AuditRecord.scala
+++ core/src/main/scala/com/outr/arango/audit/AuditRecord.scala
@@ -5,11 +5,12 @@ import fabric.Value
 import fabric.rw._
 
 case class AuditRecord(action: String,
-                       resource: String,
-                       origin: Option[String],
-                       userRef: Option[Value],
-                       value: Option[Value],
-                       metadata: Map[String, Value],
+                       resource: String = "base",
+                       origin: Option[String] = None,
+                       sessionRef: Option[Value] = None,
+                       userRef: Option[Value] = None,
+                       value: Option[Value] = None,
+                       metadata: Map[String, Value] = Map.empty,
                        created: Long = System.currentTimeMillis(),
                        _id: Id[AuditRecord] = AuditRecord.id()) extends Document[AuditRecord]
 
@@ -17,6 +18,7 @@ object AuditRecord extends DocumentModel[AuditRecord] {
   val action: Field[String] = field("action")
   val resource: Field[String] = field("resource")
   val origin: Field[Option[String]] = field("origin")
+  val sessionRef: Field[Option[Value]] = field("sessionRef")
   val userRef: Field[Option[Value]] = field("userRef")
   val value: Field[Option[Value]] = field("value")
   val metadata: Field[Map[String, Value]] = field("metadata")
@@ -29,6 +31,7 @@ object AuditRecord extends DocumentModel[AuditRecord] {
     action.index.persistent(),
     resource.index.persistent(),
     origin.index.persistent(),
+    sessionRef.index.persistent(),
     userRef.index.persistent(),
     created.index.persistent()
   )
