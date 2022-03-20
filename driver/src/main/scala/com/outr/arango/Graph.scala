e97307cff14a12fb06379ff81e17e1744e6fcf79
Luis Furnas <luisfurnas@gmail.com>
2022-03-20 16:33:03 -0100

    Remove scalafmt

--- driver/src/main/scala/com/outr/arango/Graph.scala
+++ driver/src/main/scala/com/outr/arango/Graph.scala
@@ -2,12 +2,7 @@ package com.outr.arango
 
 import cats.effect.IO
 import cats.implicits._
-import com.outr.arango.collection.{
-  Collection,
-  DocumentCollection,
-  EdgeCollection,
-  QueryBuilder
-}
+import com.outr.arango.collection.{Collection, DocumentCollection, EdgeCollection, QueryBuilder}
 import com.outr.arango.core._
 import com.outr.arango.query.{Query, Sort}
 import com.outr.arango.upgrade.{CreateDatabase, DatabaseUpgrade}
@@ -46,49 +41,38 @@ class Graph(private[arango] val db: ArangoDB) {
 
   def initialized: Boolean = _initialized.get()
 
-  def init(createDatabase: Boolean = true): IO[Unit] = if (
-    _initialized.compareAndSet(false, true)
-  ) {
+  def init(createDatabase: Boolean = true): IO[Unit] = if (_initialized.compareAndSet(false, true)) {
     for {
       _ <- CreateDatabase.upgrade(this).whenA(createDatabase)
-      appliedUpgrades <- store[AppliedUpgrades](
-        AppliedUpgrades.key,
-        _ => AppliedUpgrades.empty
-      ).map(_.labels)
-      upgrades = this.upgrades.filter(u =>
-        u.alwaysRun || !appliedUpgrades.contains(u.label)
-      )
-      _ = if (upgrades.nonEmpty)
-        scribe.info(
-          s"Applying ${upgrades.length} upgrades (${upgrades.map(_.label).mkString(", ")})..."
-        )
-      _ <- doUpgrades(
-        upgrades,
-        upgrades,
-        stillBlocking = true,
-        appliedUpgrades = Set.empty
-      )
-    } yield ()
-  } else IO.unit
+      appliedUpgrades <- store[AppliedUpgrades](AppliedUpgrades.key, _ => AppliedUpgrades.empty).map(_.labels)
+      upgrades = this.upgrades.filter(u => u.alwaysRun || !appliedUpgrades.contains(u.label))
+      _ = if (upgrades.nonEmpty) scribe.info(s"Applying ${upgrades.length} upgrades (${upgrades.map(_.label).mkString(", ")})...")
+      _ <- doUpgrades(upgrades, upgrades, stillBlocking = true, appliedUpgrades = Set.empty)
+    } yield {
+      ()
+    }
+  } else {
+    IO.unit
+  }
 
   protected def initted[Return](f: => Return): Return = {
     assert(initialized, "Database has not been initialized yet")
     f
   }
 
-  /** Creates a QueryBuilder[T] to manage execution of the supplied query.
+  /**
+    * Creates a QueryBuilder[T] to manage execution of the supplied query.
     *
     * @param query the query to create the builder for
     * @tparam T the type of results
     * @return QueryBuilder[T]
     */
-  def query[T](query: Query)(implicit rw: ReaderWriter[T]): QueryBuilder[T] =
-    this.query[T](query, rw.write _)
+  def query[T](query: Query)(implicit rw: ReaderWriter[T]): QueryBuilder[T] = this.query[T](query, rw.write)
 
-  def query[T](query: Query, converter: Value => T): QueryBuilder[T] =
-    new QueryBuilder[T](this, query, converter)
+  def query[T](query: Query, converter: Value => T): QueryBuilder[T] = new QueryBuilder[T](this, query, converter)
 
-  /** Executes the query ignoring the result. Useful for queries that modify data but don't return anything useful.
+  /**
+    * Executes the query ignoring the result. Useful for queries that modify data but don't return anything useful.
     */
   def execute(query: Query): IO[Unit] = db.query.execute(query)
 
@@ -114,19 +98,21 @@ class Graph(private[arango] val db: ArangoDB) {
       applied = appliedUpgrades + upgrade.label
       _ <- store(AppliedUpgrades.key) = AppliedUpgrades(applied)
       _ <- doUpgrades(allUpgrades, upgrades.tail, continueBlocking, applied)
-    } yield ()
+    } yield {
+      ()
+    }
 
     if (stillBlocking && !continueBlocking) {
       // Break free
       io.unsafeRunAndForget()(cats.effect.unsafe.IORuntime.global)
 
       IO.unit
-    } else io
+    } else {
+      io
+    }
   }
 
-  protected def afterStartup(upgrades: List[DatabaseUpgrade]): IO[Unit] = if (
-    upgrades.isEmpty
-  ) {
+  protected def afterStartup(upgrades: List[DatabaseUpgrade]): IO[Unit] = if (upgrades.isEmpty) {
     scribe.info("Upgrades completed successfully")
     IO.unit
   } else {
@@ -136,54 +122,29 @@ class Graph(private[arango] val db: ArangoDB) {
     }
   }
 
-  def truncate(): IO[Unit] =
-    collections.map(_.collection.truncate()).sequence.map(_ => ())
+  def truncate(): IO[Unit] = collections.map(_.collection.truncate()).sequence.map(_ => ())
 
-  def vertex[D <: Document[D]](model: DocumentModel[D]): DocumentCollection[D] =
-    synchronized {
-      val c = new DocumentCollection[D](
-        this,
-        db.collection(model.collectionName),
-        model,
-        CollectionType.Vertex
-      )
-      _collections = _collections ::: List(c)
-      c
-    }
-
-  def edge[E <: Edge[E, From, To], From, To](
-      model: EdgeModel[E, From, To]
-  ): EdgeCollection[E, From, To] = synchronized {
-    val c = new EdgeCollection[E, From, To](
-      this,
-      db.collection(model.collectionName),
-      model
-    )
+  def vertex[D <: Document[D]](model: DocumentModel[D]): DocumentCollection[D] = synchronized {
+    val c = new DocumentCollection[D](this, db.collection(model.collectionName), model, CollectionType.Vertex)
     _collections = _collections ::: List(c)
     c
   }
 
-  def view(
-      name: String,
-      links: List[ViewLink],
-      primarySort: List[Sort] = Nil,
-      primarySortCompression: SortCompression = SortCompression.LZ4,
-      consolidationInterval: FiniteDuration = 1.second,
-      commitInterval: FiniteDuration = 1.second,
-      cleanupIntervalStep: Int = 2,
-      consolidationPolicy: ConsolidationPolicy =
-        ConsolidationPolicy.BytesAccum()
-  ): View = synchronized {
-    val view = db.view(
-      name,
-      links,
-      primarySort,
-      primarySortCompression,
-      consolidationInterval,
-      commitInterval,
-      cleanupIntervalStep,
-      consolidationPolicy
-    )
+  def edge[E <: Edge[E, From, To], From, To](model: EdgeModel[E, From, To]): EdgeCollection[E, From, To] = synchronized {
+    val c = new EdgeCollection[E, From, To](this, db.collection(model.collectionName), model)
+    _collections = _collections ::: List(c)
+    c
+  }
+
+  def view(name: String,
+           links: List[ViewLink],
+           primarySort: List[Sort] = Nil,
+           primarySortCompression: SortCompression = SortCompression.LZ4,
+           consolidationInterval: FiniteDuration = 1.second,
+           commitInterval: FiniteDuration = 1.second,
+           cleanupIntervalStep: Int = 2,
+           consolidationPolicy: ConsolidationPolicy = ConsolidationPolicy.BytesAccum()): View = synchronized {
+    val view = db.view(name, links, primarySort, primarySortCompression, consolidationInterval, commitInterval, cleanupIntervalStep, consolidationPolicy)
     _views = _views ::: List(view)
     view
   }
