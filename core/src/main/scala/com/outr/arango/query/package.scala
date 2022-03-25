82b3bec5b4c1c682d9f7ac9609c3baa02cebc8a1
Matt Hicks <matt@matthicks.com>
2022-03-25 09:19:07 -0500

    Minor internal conversion fixes

--- core/src/main/scala/com/outr/arango/query/package.scala
+++ core/src/main/scala/com/outr/arango/query/package.scala
@@ -12,21 +12,22 @@ package object query {
   implicit def tuple2QueryPart(t: (String, fabric.Value)): QueryPart = QueryPart.NamedVariable(t._1, t._2)
 
   def toQueryPart(value: Any): QueryPart = value match {
-    case null => QueryPart.Variable(Null)
-    case i: Int => QueryPart.Variable(toValue(i))
-    case s: String => QueryPart.Variable(toValue(s))
-    case id: Id[_] => QueryPart.Variable(toValue(id._id))
     case qp: QueryPart => qp
-    case seq: Seq[_] => QueryPart.Variable(arr(seq.map(toValue): _*))
-    case _ => throw new RuntimeException(s"Unsupported expression: $value (${value.getClass.getName})")
+    case _ => QueryPart.Variable(toValue(value))
   }
 
   def toValue(value: Any): Value = value match {
-    case null => Null
+    case null | None => Null
+    case Some(v) => toValue(v)
+    case v: Value => v
+    case id: Id[_] => str(id._id)
     case s: String => str(s)
+    case b: Boolean => bool(b)
     case i: Int => num(i)
     case l: Long => num(l)
     case f: Float => num(f.toDouble)
     case d: Double => num(d)
+    case seq: Seq[_] => arr(seq.map(toValue): _*)
+    case _ => throw new RuntimeException(s"Unsupported expression: $value (${value.getClass.getName})")
   }
 }
