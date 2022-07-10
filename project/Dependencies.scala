edf3ea26904d07cc4440b025a40203b3d2488259
Matt Hicks <matt@courio.com>
2022-07-10 00:57:19 +0000

    Update scribe to 3.10.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val jackson: String = "3.0.1"
     val catsEffect: String = "3.3.13"
     val fs2: String = "3.2.8"
-    val scribe: String = "3.10.0"
+    val scribe: String = "3.10.1"
 
     val scalaTest: String = "3.2.12"
     val catsEffectTesting: String = "1.4.0"
