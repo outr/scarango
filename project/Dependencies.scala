ad342db9e75ebfaa7f3b733c3a554e5a4f866a7c
Matt Hicks <matt@courio.com>
2022-09-16 18:09:16 +0000

    Update fs2-core to 3.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val arangoDBJavaDriver: String = "6.18.0"
     val jackson: String = "3.0.1"
     val catsEffect: String = "3.3.14"
-    val fs2: String = "3.2.14"
+    val fs2: String = "3.3.0"
     val scribe: String = "3.10.3"
 
     val scalaTest: String = "3.2.13"
