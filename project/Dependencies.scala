33539a31adb1f30c61437923a03d9cddc9e8f238
Matt Hicks <matt@courio.com>
2022-12-04 00:43:18 +0000

    Update jackson-dataformat-velocypack to 3.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
     val profig: String = "3.4.5"
     val scalaPass: String = "1.2.4"
     val arangoDBJavaDriver: String = "6.19.0"
-    val jackson: String = "3.0.1"
+    val jackson: String = "3.1.0"
     val catsEffect: String = "3.4.1"
     val fs2: String = "3.3.0"
     val scribe: String = "3.10.5"
