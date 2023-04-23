40d2cb2b073daaf197b5da4a49bc0db262fd0e29
Matt Hicks <matt@courio.com>
2023-04-23 00:44:28 +0000

    Update cats-effect to 3.4.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val scalaPass: String = "1.2.5"
     val arangoDBJavaDriver: String = "6.21.0"
     val jackson: String = "4.0.1"
-    val catsEffect: String = "3.4.8"
+    val catsEffect: String = "3.4.9"
     val fs2: String = "3.6.1"
     val scribe: String = "3.11.1"
 
