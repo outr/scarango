06892d95cddcdf1093b17b5fe568ad210b993155
Matt Hicks <matt@matthicks.com>
2023-06-02 11:18:20 -0400

    Added Audit resource query convenience functionality

--- driver/src/main/scala/com/outr/arango/audit/AuditSupport.scala
+++ driver/src/main/scala/com/outr/arango/audit/AuditSupport.scala
@@ -1,8 +1,10 @@
 package com.outr.arango.audit
 
 import cats.effect.IO
-import com.outr.arango.collection.DocumentCollection
-import com.outr.arango.{Document, Graph}
+import com.outr.arango.collection.{DocumentCollection, QueryBuilder}
+import com.outr.arango.query.SortDirection
+import com.outr.arango.query.dsl._
+import com.outr.arango.{Document, Field, Graph}
 import fabric.Json
 import fabric.rw._
 
@@ -16,10 +18,31 @@ trait AuditSupport {
   }
 
   case class Resource(name: String) {
-    def record[T <: Document[T]: RW](action: String,
-                                               value: T,
-                                               origin: Option[String] = None,
-                                               metadata: Map[String, Json] = Map.empty): IO[AuditRecord] = {
+    def query(action: Option[String] = None,
+              origin: Option[String] = None,
+              sessionRef: Option[String] = None,
+              userRef: Option[String] = None,
+              createdAfter: Option[Long] = None,
+              createdBefore: Option[Long] = None,
+              sortField: Field[_] = AuditRecord.created,
+              sortDirection: SortDirection = SortDirection.DESC): QueryBuilder[AuditRecord] = {
+      auditLog.query.byFilter({ r =>
+        val filters = List(
+          action.map(s => r.action === s),
+          origin.map(s => r.origin === s),
+          sessionRef.map(s => r.sessionRef === s),
+          userRef.map(s => r.userRef === s),
+          createdAfter.map(l => r.created > l),
+          createdBefore.map(l => r.created < l)
+        ).flatten
+        filters.foldLeft(r.resource === name)((combined, filter) => combined && filter)
+      }, (sortField, sortDirection))
+    }
+
+    def record[T <: Document[T] : RW](action: String,
+                                      value: T,
+                                      origin: Option[String] = None,
+                                      metadata: Map[String, Json] = Map.empty): IO[AuditRecord] = {
       val record = AuditRecord(
         action = action,
         resource = name,
