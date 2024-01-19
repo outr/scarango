830a27feefd4a0f34cad9657a1d0b136fb786c30
Matt Hicks <matt@matthicks.com>
2024-01-19 15:48:38 -0600

    Minor update to allow SortDirection to be used in queries

--- core/src/main/scala/com/outr/arango/query/SortDirection.scala
+++ core/src/main/scala/com/outr/arango/query/SortDirection.scala
@@ -1,8 +1,19 @@
 package com.outr.arango.query
 
+import fabric.rw.RW
+
+import scala.language.implicitConversions
+
 sealed trait SortDirection
 
 object SortDirection {
-  case object ASC extends SortDirection
-  case object DESC extends SortDirection
+  implicit def toQueryPart(sort: SortDirection): QueryPart = QueryPart.Static(sort.toString)
+  implicit val rw: RW[SortDirection] = RW.enumeration(List(ASC, DESC), asString = _.toString)
+
+  case object ASC extends SortDirection {
+    override def toString: String = "ASC"
+  }
+  case object DESC extends SortDirection {
+    override def toString: String = "DESC"
+  }
 }
