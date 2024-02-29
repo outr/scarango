7caf5e6534f2abb29bfc766ec87496330770b810
Matt Hicks <matt@matthicks.com>
2024-02-28 20:23:21 -0600

    Replacing JsonDocument with a trait that uses JsonWrapper for better flexibility and simplification.

--- core/src/main/scala/com/outr/arango/json/JsonDocument.scala
+++ core/src/main/scala/com/outr/arango/json/JsonDocument.scala
@@ -3,4 +3,8 @@ package com.outr.arango.json
 import com.outr.arango.{Document, Id}
 import fabric._
 
-case class JsonDocument(json: Json, _id: Id[JsonDocument]) extends Document[JsonDocument]
+/**
+  * JsonDocument is a convenience class that extends Document and JsonWrapper to provide a simple way to work with
+  * ArangoDB documents that are stored as JSON.
+  */
+trait JsonDocument extends Document[JsonDocument] with JsonWrapper
