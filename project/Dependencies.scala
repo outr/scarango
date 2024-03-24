077cfaf54925456910c325b9aa7b84179d01bc20
Matt Hicks <matt@courio.com>
2024-03-24 00:42:29 +0000

    Update arangodb-java-driver to 7.6.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     
     val scalaPass: String = "1.2.8"
 
-    val arangoDBJavaDriver: String = "7.5.1"
+    val arangoDBJavaDriver: String = "7.6.0"
 
     val jackson: String = "4.2.0"
     
